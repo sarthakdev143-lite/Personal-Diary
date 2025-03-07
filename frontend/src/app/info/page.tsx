5b53c31fe819e9c28c6a65d50e29f3162da1ff0f
sarthakdev143 <sarthakdev143.official@gmail.com>
2025-03-07 22:20:12 +0530

    Fixed ESLint Errors

--- frontend/src/app/info/page.tsx
+++ frontend/src/app/info/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 // import { Button } from '@/components/ui/button';
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
