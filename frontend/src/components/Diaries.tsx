d5a5fce39ded7290095bb1a2d65138b255e9d434
sarthakdev143-lite <sarthakdev143.official@gmail.com>
2025-04-14 01:29:08 +0530

    Remove deprecated packages and comment out unused code in Diaries component

--- frontend/src/components/Diaries.tsx
+++ frontend/src/components/Diaries.tsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-type Diary = {
-    id: number;
-    title: string;
-    content: string;
-};
+// type Diary = {
+//     id: number;
+//     title: string;
+//     content: string;
+// };
 
 const Diaries = () => {
-    const [diaries, setDiaries] = useState<Diary[]>([]);
+    // const [diaries, setDiaries] = useState<Diary[]>([]);
 
     return (
         <>
-            {
+            {/* {
                 diaries.forEach((diary) => {
                     // Display Diary Info.
                 })
-            }
+            } */}
         </>
     )
 }
